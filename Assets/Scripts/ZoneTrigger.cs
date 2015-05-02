e532dcd1cea0e0418b2c89824ffb3d46a26b6953
Remorc Threak the Destroyer of Worlds <kpawla14@student.aau.dk>
2015-05-02 16:58:29 +0200

    Player controls, zonetriggers w/ player- & impulsecount

---
+++ Assets/Scripts/ZoneTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneTrigger : MonoBehaviour {
+    public int counter;
+
+    public int impulseCounter;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

7be76dedf6e0a17e75ef4a40f8a0f4b7f376314a
Wikzo <Linkthewindwaker@gmail.com>
2015-05-03 10:26:15 +0200

    Platform level

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour
+{
+
+    private Transform _transform;
+    public float Speed = 10f;
+
+    private bool Ready = false;
+
+    // Use this for initialization
+    private void Start()
+    {
+        _transform = transform;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            Ready = true;
+
+        if (!Ready)
+            return;
+
+        transform.Translate(Vector3.right * Speed * Time.deltaTime);
+    }
+}
